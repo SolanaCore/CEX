522063e26239c1cfacfcbd1f665c98e8a62021bc
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-08-18 00:01:12 +0530

    update: Use the built-in regex feature of the validate crate

--- api/src/types/trades.rs
+++ api/src/types/trades.rs
@@ -1,9 +1,9 @@
 use validate::Validator;
 use serde::Deserialize;
-use crate::utils::{MIN_CONST, MAX_CONST, UTC_REGEX}
+use crate::utils::{MIN_CONST, MAX_CONST, UTC_REGEX, SYMBOL_REGEX}
 #[derive(Deserialize, Validator)]
 pub(crate) struct Request {
-    #[validate(length(min = "MIN_CONST", max = "MAX_CONST"))]
+    #[validate(regex(path = *SYMBOL_REGEX))]
     symbol: String,
     limit: Option<u16>,  //User may or may not provide the limit, so we take 50 as default and 999 max..
     #[validate(regex(path = *UTC_REGEX))]
@@ -15,25 +15,13 @@ pub(crate) struct Request {
 
 #[derive(Deserialize, Validator)]
 pub(crate) struct Request {
-    #[validate(custom(function = "is_valid_symbol"))]
+    #[validate(regex(path = *SYMBOL_REGEX))]
     symbol: String
 }
 
 
 #[derive(Deserialize, Validator)]
 pub(crate) struct Request {
-    #[validate(custom(function = "is_valid_symbol"))]
+    #[validate(regex(path = *SYMBOL_REGEX))]
     symbol: String
-}
-
-
-pub fn is_valid_symbol(symbol: &str) -> Result<(), ValidationError> {
-    // Regex: only allows uppercase letters + "/" + uppercase letters
-    let re = Regex::new(r"^[A-Z]+/[A-Z]+$").unwrap();
-
-    if re.is_match(symbol) {
-        Ok(())
-    } else {
-        Err(ValidationError::new("invalid_symbol"))
-    }
 }
