7539fc432b7ae6d0c10bfa4a8d54b491732ec15b
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-08-22 22:34:14 +0530

    removed: use validator crate instead of is_validate_utc_time...

--- api/src/routes/trades/historic_trades.rs
+++ api/src/routes/trades/historic_trades.rs
@@ -14,18 +14,4 @@ async fn historic_trades(data: web::json<Request>) {
     let limit = data.limit.unwrap_or(100);
         HttpResponse::Ok().finish()
 
-         if !validate_utc_time_format(&data.start_time) {
-        return HttpResponse::BadRequest().json("Invalid start_time format. Expected format: YYYY-MM-DDTHH:mm:ssZ");
-    }
-
-    let end_time = if let Some(end_time_str) = &data.end_time {
-        if !validate_utc_time_format(end_time_str) {
-            return HttpResponse::BadRequest().json("Invalid end_time format. Expected format: YYYY-MM-DDTHH:mm:ssZ");
-        }
-        end_time_str.clone()
-    } else {
-        //RFC 3339 is a standard for representing date and time in a human-readable string format, based on ISO 8601. It is commonly used in APIs and data interchange.
-        Utc::now().to_rfc3339()
-    };
-
 }
