9aef261a404fe52da7813ed273d98dc076959db9
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-08-23 23:54:13 +0530

    feat(postgres): Add conn field to struct to avoid repeatedly passing connection in function calls

--- postgres-db/src/utils.rs
+++ postgres-db/src/utils.rs
@@ -4,58 +4,55 @@ use crate::models::{User, NewUser, Order, NewOrder, UserBalance, NewUserBalance}
 use uuid::Uuid;
 use crate::schema::{users, orders, user_balances};
 use diesel::result::Error;
+use crate::connection::{DB_POOL, DbPool};
 
-pub struct DatabaseInvoker;
+pub struct PostgresInvoker {
+    pub conn: DbPool,
+}
 
-impl DatabaseInvoker {
-    // Insert a new user into the users table
-    pub fn add_user<'a>(conn: &PgConnection, new_user: NewUser<'a>) -> Result<User, Error> {
+impl PostgresInvoker {
+     pub fn new() -> Self {
+        Self {
+            pool: DB_POOL.clone(),
+        }
+    }
+
+    pub fn add_user<'a>(&self, new_user: NewUser<'a>) -> Result<User, Error> {
+        let mut conn = self.conn.get()?;
         diesel::insert_into(users::table)
             .values(&new_user)
-            .get_result(conn)
-            .map_err(|e| e)  // Propagate any errors
+            .get_result(&mut conn)
     }
 
-    // Retrieve a user by user_id
-    pub fn get_user(conn: &PgConnection, user_id: Uuid) -> Result<User, Error> {
-        users::table
-            .filter(users::id.eq(user_id))
-            .first(conn)
-            .map_err(|e| e)  // Propagate errors, such as user not found
+    pub fn get_user(&self, user_id: Uuid) -> Result<User, Error> {
+        let mut conn = self.conn.get()?;
+        users::table.filter(users::id.eq(user_id)).first(&mut conn)
     }
 
-    // Insert a new order into the orders table
-    pub fn add_order<'a>(conn: &PgConnection, new_order: NewOrder<'a>) -> Result<Order, Error> {
+    pub fn add_order<'a>(&self, new_order: NewOrder<'a>) -> Result<Order, Error> {
+        let mut conn = self.conn.get()?;
         diesel::insert_into(orders::table)
             .values(&new_order)
-            .get_result(conn)
-            .map_err(|e| e)
+            .get_result(&mut conn)
     }
 
-    // Retrieve all orders for a specific user
-    pub fn get_user_orders(conn: &PgConnection, user_id: Uuid) -> Result<Vec<Order>, Error> {
-        orders::table
-            .filter(orders::user_id.eq(user_id))
-            .load::<Order>(conn)
-            .map_err(|e| e)
+    pub fn get_user_orders(&self, user_id: Uuid) -> Result<Vec<Order>, Error> {
+        let mut conn = self.conn.get()?;
+        orders::table.filter(orders::user_id.eq(user_id)).load(&mut conn)
     }
 
-    // Retrieve the user balance
-    pub fn get_user_balance(conn: &PgConnection, user_id: Uuid) -> Result<Vec<UserBalance>, Error> {
-        user_balances::table
-            .filter(user_balances::user_id.eq(user_id))
-            .load::<UserBalance>(conn)
-            .map_err(|e| e)
+    pub fn get_user_balance(&self, user_id: Uuid) -> Result<Vec<UserBalance>, Error> {
+        let mut conn = self.conn.get()?;
+        user_balances::table.filter(user_balances::user_id.eq(user_id)).load(&mut conn)
     }
 
-    // Insert new user balance data
     pub fn add_user_balance<'a>(
-        conn: &PgConnection,
+        &self,
         new_user_balance: NewUserBalance<'a>,
     ) -> Result<UserBalance, Error> {
+        let mut conn = self.conn.get()?;
         diesel::insert_into(user_balances::table)
             .values(&new_user_balance)
-            .get_result(conn)
-            .map_err(|e| e)
+            .get_result(&mut conn)
     }
 }
