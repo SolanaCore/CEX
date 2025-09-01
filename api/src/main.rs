c080704d859888cd18c6fc49ac0bee199c99295b
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-09-01 17:32:58 +0530

    Update main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,10 +2,11 @@ use actix_web::{middleware::Logger, web, App, HttpServer, HttpResponse, Responde
 use dotenv::dotenv;
 use postgres::connection::DB_POOL;
 use actix_web::web::Data;
+use crate::routes;
 //TODO: CORS
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
     env_logger::init();
 
@@ -16,13 +17,11 @@ async fn main() -> std::io::Result<()> {
             .app_data(Data::new(db_pool.clone()))
             .wrap(Logger::default())
             .service(
-                web::scope("/api")
-                .configure(api::routes::routes_config)
-                
+                web::scope("/api/v1")
+                .configure(routes::routes_config)
             )
-            .configure(api::routes::routes_config)
     })
-    .bind(())?
+    .bind(())
     .run()
     .await;
 }
